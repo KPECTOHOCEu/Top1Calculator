b6802895f40f4b3bfecf55a66cc8500c85f39621
xNeonShadow <124272727+KPECTOHOCEu@users.noreply.github.com>
2024-02-12 20:59:46 +0300

    Add files via upload

---
+++ Top1Calculator/target/debug/deps/calculator-035ee91ca82deaa7.d
@@ -0,0 +1,5 @@
+C:\Projects\Rust\calculator\target\debug\deps\calculator-035ee91ca82deaa7.exe: src\main.rs
+
+C:\Projects\Rust\calculator\target\debug\deps\calculator-035ee91ca82deaa7.d: src\main.rs
+
+src\main.rs:
